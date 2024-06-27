227c7653074545d5da8a68fb866b0e8db86e458d
Sreshth <sreshthtiwari13@gmail.com>
2024-06-27 17:44:42 +0530

    hashmap

--- Two Sum (Easy)/main.py
+++ Two Sum (Easy)/main.py
@@ -1,4 +1,4 @@
-# O(n^2)
+# O(n^2) bruteforce
 
 def twoSum(nums, target):
     for i in range(len(nums)):
@@ -6,4 +6,15 @@ def twoSum(nums, target):
             if i!=j and nums[i] + nums[j]==target:
                 return [i, j]
             
-print("solution: ", twoSum([3, 2], 6))
+# O(n) hashmap
+
+def twosum(nums, target):
+    hash_ = {}
+    for idx, num in enumerate(nums):
+        if target - num in hash_:
+            print(idx, target, num, hash_)
+            return [hash_[target - num], idx]
+        hash_[num] = idx
+        print(hash_)
+
+print("solution: ", twosum([2, 3, 43, 5], 7))
